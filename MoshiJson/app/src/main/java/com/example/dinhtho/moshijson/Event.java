fa7228420be40795fc0978f428b38dffbb31a927
THO NGUYEN <dinhthohcmus@gmail.com>
2017-03-24 19:03:36 +0700

    parse Json with Moshi

---
+++ MoshiJson/app/src/main/java/com/example/dinhtho/moshijson/Event.java
@@ -0,0 +1,12 @@
+package com.example.dinhtho.moshijson;
+
+import java.io.Serializable;
+
+/**
+ * Created by dinhtho on 24/03/2017.
+ */
+
+public class Event implements Serializable {
+    String title;
+    String beginDateAndTime;
+}
