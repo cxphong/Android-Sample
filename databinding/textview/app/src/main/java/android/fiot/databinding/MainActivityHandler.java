5af961dc996b538fbd80d5a272aeaba61063857e
Cao Xuan Phong <>
2017-12-05 12:40:14 +0700

    Update databinding

--- databinding/textview/app/src/main/java/android/fiot/databinding/MainActivityHandler.java
+++ databinding/textview/app/src/main/java/android/fiot/databinding/MainActivityHandler.java
@@ -5,6 +5,8 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 
+import java.util.Random;
+
 /**
  * Created by caoxuanphong on 12/5/17.
  */
@@ -14,6 +16,21 @@ public class MainActivityHandler {
 
     public void onClickUserName(View view, User user) {
         Log.d(TAG, "onClickUser " + view + ", " + user.getName());
+
+        // Update model & view
+        user.setName(getSaltString());
     }
 
+    protected String getSaltString() {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 18) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+
+    }
 }
--- databinding/textview/app/src/main/java/android/fiot/databinding/User.java
+++ databinding/textview/app/src/main/java/android/fiot/databinding/User.java
@@ -1,5 +1,8 @@
 package android.fiot.databinding;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.Observable;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -8,15 +11,19 @@ import android.util.Log;
  * Created by caoxuanphong on 12/5/17.
  */
 
-public class User {
+public class User extends BaseObservable {
     private static final String TAG = "User";
     private String name = "Phong Cao";
 
+    @Bindable
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
 
+    @Bindable
     public String getName() {
         return name;
     }
+
 }
